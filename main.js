7c57de5b2e8cb6e80ce894d4c67363d4f8c8b198
asaad227 <a.saad227@yahoo.com>
2021-12-22 16:47:38 +0000

    testing array functionality

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 /* 
 👉 Write your kata here!
 */
+let magicalFlowers = [1, 4, 3, -4]
+
+function arrayLoop(array){
+
+    for(let i= 0; i < array.length; i++){
+        array[i];
+
+    }
+
+    return array;
+}
+
+/*function bunchTogetherFlowersByNumber(array){
+    let evenNumFlowers=[];
+    let oddNumFlowers=[];
+    let negativeNumFlowers=[];
+    for(let i = 0; i < magicalFlowers.length; i++){
+   if(magicalFlowers[i]% 2 === 0){
+      evenNumFlowers.push(magicalFlowers[i])
+   } else if(magicalFlower[i] > 0){
+       negativeNumFlowers.push(magicalFlowers[i])
+   } else { oddNumFlowers.push(magicalFlowers[i])
+   }
+   return evenNumFlowers;
+   //return negativeNumFlowers;
+   //return oddNumFlowers;
+    }
+}
+
+
+}
+*/
 
 //👉 Write the function your CodeWarriors will start with below here:
--- main.test.js
+++ main.test.js
@@ -1,1 +1,69 @@
 //👉 Write your tests below here:
+/*function sum(a, b){
+
+    return a+b
+}
+
+it("should add the number", function(){
+
+    const actual = sum(2,3)
+   
+    const expected = 5
+    expect(actual).toBe(expected);
+})*/
+ 
+function arrayLoop(array){
+ const evenArray = [];
+ const oddArray  = [];
+const negetiveArray = [];
+ 
+    for(let i= 0; i < array.length; i++){
+       
+        if(array[i] < 0){
+            negetiveArray.push(array[i]);
+
+        }
+        if(array[i]%2 === 1){
+            oddArray.push(array[i]);
+
+        }
+        if(array[i]%2 === 0){
+            evenArray.push(array[i]);
+
+        }
+
+
+
+    }
+
+    return oddArray,
+    evenArray,
+    negetiveArray;
+    
+}
+describe("Array functionality testing.", () =>{
+it("should have even number array", function(){
+    const newArray = [1, 2, 3, -1];
+    const actual = arrayLoop(newArray);
+   
+    const expected = [2];
+    expect(actual).toStrictEqual(expected);
+})
+
+it("should have odd number array", function(){
+    const newArray = [1, 2, 3, -1];
+    const actual = arrayLoop(newArray);
+   
+    const expected = [2];
+    expect(actual).toStrictEqual(expected);
+})
+
+it("should have negetive number array", function(){
+    const newArray = [1, 2, 3, -1];
+    const actual = arrayLoop(newArray);
+   
+    const expected = [-1];
+    expect(actual).toStrictEqual(expected);
+})
+
+});
